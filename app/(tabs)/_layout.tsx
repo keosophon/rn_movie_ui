aaed79ff9e3829587d9c657daf52ede1e8793d52
keosophon@gmail.com <keosophon8888@gmail.com>
2025-07-26 15:13:10 +1200

    screen headerShown not shown

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,10 @@
+import { Tabs } from 'expo-router'
+import React from 'react'
+
+const _layout = () => {
+  return (
+    <Tabs screenOptions={{ headerShown: false }}/>
+  )
+}
+
+export default _layout
