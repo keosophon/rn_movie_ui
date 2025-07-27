c7d4d4444b80a71ac306323b449866d360afb05a
keosophon@gmail.com <keosophon8888@gmail.com>
2025-07-28 11:02:02 +1200

    search component

---
+++ app/components/SearchBar.tsx
@@ -0,0 +1,25 @@
+import { icons } from '@/constants/icons';
+import React from 'react';
+import { Image, TextInput, View } from 'react-native';
+
+interface Props {
+  onPress?: () => void;
+  placeholder: string;
+}
+
+const SearchBar = ({onPress, placeholder}:Props) => {
+  return (
+    <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'>
+      <Image source={icons.search} className='size-5' resizeMode = 'contain' tintColor='#ab8bff'/>
+      <TextInput
+        placeholder={placeholder}
+        placeholderTextColor='#a8b5db'
+        className='flex-1 text-white ml-2'
+        value = ''
+        onChangeText={() => {}}
+        onPress={onPress} />
+    </View>
+  )
+}
+
+export default SearchBar
